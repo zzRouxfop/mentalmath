43b062bbe8a5bee526c18a6c58d124c168d79079
zzRxoufop <shuffleitupyourass@gmail.com>
2025-04-11 13:02:30 -0400

    couple tweaks

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import random
 
 def digit_generator(hmd_fd, hmd_sd): # hmd_fd = how many digits for first digit, hmd_sd = how many digits for second digit
-    fd = int("".join([str(random.randint(0,9)) for j in range(hmd_fd)]))
-    sd = int("".join([str(random.randint(0,9)) for j in range(hmd_sd)]))
+    fd = "".join([str(random.randint(0,9)) for j in range(hmd_fd)])
+    sd = "".join([str(random.randint(0,9)) for j in range(hmd_sd)])
     
-    if fd == "":
-        fd = 0
-    if sd == "":
-        sd = 0
+    if fd == "0":
+        fd = int(fd)
+    else:
+        fd = int(fd.lstrip('0'))
+        
+    if sd == "0":
+        sd = int(sd)
+    else:
+        sd = int(sd.lstrip('0'))
         
     return [fd, sd]
 
@@ -30,7 +35,17 @@ def printing_out(problem, operation):
     elif operation == 3:
         return str(max(problem)) + ' * ' + str(min(problem)) + " = "
 
-        
+def quizzing(problem_set, answer_key):
+    counter = 0
+    lst_marker = 0
+    for l in problem_set:
+        print(f"{str(lst_marker + 1)}. {printing_out(l, oper)}")
+        answer = int(input("type answer: "))
+        if answer == answer_key[lst_marker]:
+            counter += 1
+        lst_marker += 1
+    return counter
+    
 if __name__ == "__main__":
     q = int(input("how many problems?: "))
     fd = int(input("how many digits for first digit?\n>>> "))
@@ -40,16 +55,9 @@ if __name__ == "__main__":
     problem_set = number_of_problems(q, fd, sd)
     answer_key = answerkey(problem_set, oper)
     
-    counter = 0
-    lst_marker = 0
-    for l in problem_set:
-        print(f"{str(lst_marker + 1)}. {printing_out(l, oper)}")
-        answer = int(input("type answer: "))
-        if answer == answer_key[lst_marker]:
-            counter += 1
-        lst_marker += 1
-        
-    print(f"final score: {counter / q * 100} % ({counter} / {q})")
+    correct_answers = quizzing(problem_set, answer_key)
+    
+    print(f"final score: {round(correct_answers / q * 100, 2)} % ({correct_answers}/{q})")
     
     # ideas:
     #     -timer and average time per problem
