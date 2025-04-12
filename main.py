bdd4eed999679dab2078a8b9bcd934ae9329d1eb
zzRxoufop <shuffleitupyourass@gmail.com>
2025-04-12 02:37:42 -0400

    added time elapsed and average time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def digit_generator(hmd_fd, hmd_sd): # hmd_fd = how many digits for first digit, hmd_sd = how many digits for second digit
     fd = "".join([str(random.randint(0,9)) for j in range(hmd_fd)])
@@ -34,17 +35,36 @@ def printing_out(problem, operation):
         return str(max(problem)) + ' - ' + str(min(problem)) + " = "
     elif operation == 3:
         return str(max(problem)) + ' * ' + str(min(problem)) + " = "
-
+    
+def get_ready():
+    print("ready?")
+    time.sleep(3)
+    print("3...")
+    time.sleep(1)
+    print("2...")
+    time.sleep(1)
+    print('1...')
+    time.sleep(1)
+    print("go!\n")
+    
 def quizzing(problem_set, answer_key):
-    counter = 0
+    correct = 0
     lst_marker = 0
+    ttp = [] # time per problem
+    
     for l in problem_set:
+        ts_ep = time.time() # time start for each problem
         print(f"{str(lst_marker + 1)}. {printing_out(l, oper)}")
         answer = int(input("type answer: "))
         if answer == answer_key[lst_marker]:
-            counter += 1
+            correct += 1
         lst_marker += 1
-    return counter
+        te_ep = time.time() # time end for each problem
+        ttp.append(int(te_ep - ts_ep))
+    
+    attp = round(sum(ttp) / len(ttp), 1)
+        
+    return correct, attp 
     
 if __name__ == "__main__":
     q = int(input("how many problems?: "))
@@ -55,11 +75,16 @@ if __name__ == "__main__":
     problem_set = number_of_problems(q, fd, sd)
     answer_key = answerkey(problem_set, oper)
     
-    correct_answers = quizzing(problem_set, answer_key)
+    get_ready()
+    
+    time_start = time.time()
+    correct_answers, avg_time_per_problem = quizzing(problem_set, answer_key)
+    time_finish = time.time()
     
     print(f"final score: {round(correct_answers / q * 100, 2)} % ({correct_answers}/{q})")
+    print(f"elapsed time: {int(time_finish - time_start)}")
+    print(f"average time per problem: {avg_time_per_problem}")
     
     # ideas:
-    #     -timer and average time per problem
     #     -stats sheet
     #     -random operation per problem?
